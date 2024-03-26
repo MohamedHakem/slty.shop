e302659b50e19b76b9445d2a754b9b56cdf44448
Mohamed Hakem <mohamedhakem959628@gmail.com>
2024-03-26 16:22:40 +0200

    fix: remove unnecessary props (auto-added and removed before 3 times?!)

--- app/(homepage)/(basic-pages)/contact-us/page.tsx
+++ app/(homepage)/(basic-pages)/contact-us/page.tsx
@@ -1,6 +1,6 @@
 import { CategoriesNav } from "@/components/navigation/categories-nav";
 
-export default async function ContactPage(props) {
+export default async function ContactPage() {
   return (
     <div dir="rtl" className="w-full flex flex-col items-center pr-4 md:px-4 border-t">
       <CategoriesNav />
