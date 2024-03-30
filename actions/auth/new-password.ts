131ec784a462474303499edd7c89c41b0f4e1d8d
MohamedHakem <mohamedhakem959628@gmail.com>
2024-03-30 10:23:44 +0200

    fix(deploy): Fix vercel error

--- actions/auth/new-password.ts
+++ actions/auth/new-password.ts
@@ -4,7 +4,7 @@ import * as z from "zod";
 import bcrypt from "bcryptjs";
 
 import { NewPasswordSchema } from "@/schemas";
-import { getPasswordResetTokenByToken } from "@/data/password-reset-token";
+import { getPasswordResetTokenByToken } from "@/data/auth/password-reset-token";
 import { getUserByEmail } from "@/data/user";
 import { db } from "@/lib/db";
 
