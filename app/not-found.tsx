3bb8fe493aa6a06f52fe28ffcba2bb8674ee9d5c
MohamedHakem <mohamedhakem959628@gmail.com>
2024-05-30 22:53:05 +0300

    style: make not-found page responsive/pretty on all screens

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,11 +14,11 @@ export default async function NotFound() {
         </video>
 
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white font-bold">
-          <div className="w-[520px] h-72 justify-center items-center text-center flex flex-col rounded-3xl text-white gap-8 bg-slate-400/80 backdrop-blur-2xl p-4">
-            <h2 className="text-4xl font-bold">Not Found!</h2>
-            <p className="text-2xl font-bold">Could not find requested resource</p>
+          <div className="w-3/4 md:w-[520px] h-56 md:h-72 justify-center items-center text-center flex flex-col rounded-3xl text-white gap-6 md:gap-8 bg-slate-400/80 backdrop-blur-2xl p-4">
+            <h2 className="text-3xl md:text-4xl font-bold">Not Found!</h2>
+            <p className="text-base md:text-2xl font-bold">Could not find requested resource</p>
             <div>
-              <span className="mr-8 text-2xl font-bold">Go to</span>
+              <span className="mr-8 text-xl md:text-2xl font-bold">Go to</span>
               <Link href="/">
                 <Button className="text-white" variant={"default"} size={"xl"}>
                   <span className="font-bold text-2xl">Home</span>
