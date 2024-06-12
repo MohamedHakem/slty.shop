1a1f821027c1baf7e130f7505d0e2de561f51508
MohamedHakem <mohamedhakem959628@gmail.com>
2024-06-12 18:12:30 +0000

    refactor: initialize redis/ratelimit outside middleware func

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,18 @@ import { chain } from "@/middlewares/chain";
 import { withAuthMiddleware } from "@/middlewares/withAuthMiddleware";
 import { withI18nMiddleware } from "@/middlewares/withI18nMiddleware";
 import { withRateLimitMiddleware } from "@/middlewares/withRateLimitMiddleware";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.cachedFixedWindow(10, "10 s"),
+  ephemeralCache: new Map(),
+  prefix: "ratelimit",
+});
 
 export default chain([
-  withRateLimitMiddleware,
+  withRateLimitMiddleware(ratelimit),
   withI18nMiddleware,
   withAuthMiddleware,
 ]);
--- middlewares/withRateLimitMiddleware.ts
+++ middlewares/withRateLimitMiddleware.ts
@@ -1,26 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { CustomMiddleware } from "@/middlewares/chain";
-import { ratelimit } from "@/lib/upstash/redis";
 import { getLocale } from "@/lib/getLocale";
 import { getMessageForRateLimited } from "@/lib/getMessageForRateLimited";
+import { Ratelimit } from "@upstash/ratelimit";
 
-export function withRateLimitMiddleware(
-  middleware: CustomMiddleware,
-): CustomMiddleware {
-  return async (
-    request: NextRequest,
-    event: NextFetchEvent,
-    response: NextResponse,
-  ) => {
-    const ip = request.ip ?? "127.0.0.1";
-    const { success } = await ratelimit.limit(ip);
-    const isPosthog = request.nextUrl.pathname.startsWith("/ingest");
-    console.log("🚀 withRateLimitMiddleware ~ isPosthog: ", isPosthog, ", pathname: ", request.nextUrl.pathname);
-    if (!success && !isPosthog) {
-      const message = await getMessageForRateLimited(getLocale(request));
-      return new Response(message, { status: 429 }); // or redirect to a /rate-limited page
-    }
-    return middleware(request, event, response);
+export function withRateLimitMiddleware(ratelimit: Ratelimit) { // HOF / Closure
+  return function (middleware: CustomMiddleware): CustomMiddleware {
+    return async (
+      request: NextRequest,
+      event: NextFetchEvent,
+      response: NextResponse,
+    ) => {
+      const ip = request.ip ?? "127.0.0.1";
+      const { success } = await ratelimit.limit(ip);
+      const isPosthog = request.nextUrl.pathname.startsWith("/ingest");
+      console.log(
+        "🚀 withRateLimitMiddleware ~ isPosthog: ",
+        isPosthog,
+        ", pathname: ",
+        request.nextUrl.pathname,
+      );
+      if (!success && !isPosthog) {
+        const message = await getMessageForRateLimited(getLocale(request));
+        return new Response(message, { status: 429 }); // or redirect to a /rate-limited page
+      }
+      return middleware(request, event, response);
+    };
   };
 }
